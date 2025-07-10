5489f42aaa872660d6f3da178a7ac6bf7beef13d
Lina Chioma Anaso <linachiomaanaso@mac.localdomain>
2025-07-10 05:54:25 +0200

    Add Layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import Home from "./component/Pages/Home"
 import About from "./component/Pages/About"
 import Vans from "./component/Pages/Vans/Vans"
 import VansDetail from "./component/Pages/Vans/VansDetail"
-import Layout from "../src/component/Pages/Layout"
+import Layout from "./component/Layout"
 import Dashboard from "../src/component/Pages/Host/Dashboard"
 import Income from "../src/component/Pages/Host/Income"
 import Reviews from "../src/component/Pages/Host/Reviews"
+import HostLayout from "./component/HostLayout";
 //import ".server"
 
 
@@ -17,14 +18,18 @@ function App() {
   return (
     <>
       <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/vans" element={<Vans />} />
-          <Route path="/vans/:id" element={<VansDetail />} />
-          <Route path="/host" element={<Dashboard />} />
-          <Route path="/host/income" element={<Income />} />
-          <Route path="/host/reviews" element={<Reviews />} />
+        <Route path ="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VansDetail />} />
+
+          <Route path="host" element={<HostLayout />} >
+              <Route index element={<Dashboard />} >
+              <Route path="income" element={<Income />} />
+              <Route path="reviews" element={<Reviews />} />
+          </Route>
+          </Route>
         </Route>
         
       </Routes>
--- src/component/Pages/Host/Dashboard.jsx
+++ src/component/Pages/Host/Dashboard.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Outlet } from "react-router-dom"
 
 function Dashboard() {
   return (
     <div>
       <h1>Host Dashboard here!</h1>
+      <Outlet />
     </div>
   )
 }
